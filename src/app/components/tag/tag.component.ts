15b67437938f017af4b27f7a0f4c18500ed6ae99
Sergio <sergio.costa@outlook.com>
2019-12-03 14:41:47 -0300

    small changes to tag component

--- src/app/components/tag/tag.component.ts
+++ src/app/components/tag/tag.component.ts
@@ -8,9 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TagComponent implements OnInit {
 
   @Input() label: string
-  @Input() color: string
   @Input() margin: string = 'left'
- 
+
   constructor() { }
 
   ngOnInit() {}
